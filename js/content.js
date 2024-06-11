e619f38723baea3c84d6b32732b2024f24d63777
zwj0914 <1134542890@qq.com>
2024-06-11 17:04:55 +0800

    update1
    xj

--- js/content.js
+++ js/content.js
@@ -13,7 +13,7 @@ function doTips() {
     setInterval(() => {
         if (countDown === 0) {
             if (needTips) return
-            window.location.href="https://invite.go2money.xyz"
+         
         } else {
             countDown--;
             node.innerText = `${countDown}s后跳转到下一页面`
